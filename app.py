3795f4cc0b8759f719dd9cc39a78dc12e0f37a30
Dhruv Ahuja <dhruvahuja2k@gmail.com>
2021-07-22 16:45:02 +0530

    'Optimized'

--- app.py
+++ app.py
@@ -12,54 +12,30 @@ ia = IMDb()
 
 #now I will try to make classes and setup the base for the app
 
-class search:
-
-    
+class app:
     def welcome(self):
         
         ask_move = input('What would you like to do?\nPress 1 to enter a Netflix show into the local database.\nPress 2 to view the shows stored in the local database.\nPress 3 to check for show updates.\nWrite quit to quit:  ')
+        
+        return ask_move
+        
 
-        if ask_move == '1':
-            pass
-        
-        elif ask_move == '2':
-            print(table().show_db())
-            system('pause')
-            self.ask_save()
-            
-        elif ask_move == '3':
-            print(check().check_season())
-            # system('pause')
-            self.ask_save()
-        
-        elif ask_move == 'quit':
-            sys.exit()
-        
-        else:
-            print('\n\nEnter a valid selection!\n\n')
-            self.welcome()
-    
-        
     def ask_input(self):
-        self.welcome()
-        
         
         usr_input = input('Enter the TV Show to search for(please try to be accurate!!): ')
         
         self.show_search = ia.search_movie(usr_input)
         
         print(f'You have entered {usr_input}. Now scouring our online databases.')
+        return self.show_search
         
+    def results(self, show_search):
         
-    def results(self):
-        self.ask_input()
         print("Showing the most relevant results:")
         print()
         count = 1
         
-        entry = self.show_search
-        
-        
+        entry = show_search
         
         for series in entry:
             yr = 'year'
@@ -68,7 +44,7 @@ class search:
                 
                 print(f'{count}. {series}, {series["year"]}')
                 count += 1
-                      
+                    
     
     
     def choose(self):
@@ -93,103 +69,88 @@ class search:
             return selection
             
             
-            
         elif select1 == 0:
             print('Returning you back to the main dialogue.')
             
             return self.ask_save()
-            
+    
+    def getselection(self):
+        return self.choose()
+    
         
         
+class webscraper(app):
+    def __init__(self):
+        app.__init__(self)
         
+        
+    
     def get_seasons(self):
-        self.choose()
-            
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('log-level=2')
         
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options = chrome_options)
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
         
-        self.query = f"{self.selection} Netflix"
-
-        
+        query = f'Peaky Blinders Netflix'
         links = []
+        url =  f"http:\\google.com/search?q={query}&start="
+        driver.get(url)
         
-        for site in range(1):
-            url = f"http:\\google.com/search?q={self.query}&start="
-            driver.get(url)
-            
-            soup = BeautifulSoup(driver.page_source, 'lxml')
-            
-            #this is google's current search result link class
-            
-            search = soup.find_all('div', class_ = 'yuRUbf')
-
-            for h in search:
-                links.append(h.a.get('href'))
-            
-        for site in links:
-            show_url = site
-            break
-
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        search = soup.find_all('div', class_ = 'yuRUbf')
+        for h in search:
+            links.append(h.a.get('href'))
+        
+        show_url = links[0]
+        
+        driver.get(show_url)
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        search2 = soup.find_all('span',attrs={'class' : 'test_dur_str'})
         
         parsed = []
+        for page in search2:
+            parsed.append(page.text[:2])
         
-        #now going to go to the netflix page of the show and grab the season count
+        parsed = [int(str(i.strip())) for i in parsed]
+        self.season_count = season_count = parsed[0]
         
-        for page in range(1):
-            url = show_url
-            driver.get(url)
+        # print(f'Number of seasons: {season_count} of show: {self.getselection()}') 
+        return season_count
+
+
+    # def ask_save(self):
+    #     self.get_seasons()
+        
+    #     ans = input('Do you want to save the show to the database? Enter "yes" to confirm or "no" to go back to main prompt: ')
+        
+    #     if ans == 'no':
+    #         self.ask_save()
             
-            soup = BeautifulSoup(driver.page_source, 'lxml')
-            search = soup.find_all('span',attrs={'class' : 'test_dur_str'})
-            
-              
-        for res in search:
-            parsed.append(res.text[:2])
-            print(res.text)
-        
-        parsed = [i.strip() for i in parsed]
-        parsed = [int(i) for i in parsed]
-        
-        self.season_count = season_count = int()
-        for i in parsed:
-            season_count = i
-            break
-        
-        print(f'Number of seasons: {season_count} of show: {self.selection}') 
-        
-        
-    def ask_save(self):
-        self.get_seasons()
-        
-        ans = input('Do you want to save the show to the database? Enter "yes" to confirm or "no" to go back to main prompt: ')
-        
-        if ans == 'no':
-            self.ask_save()
-            
-        else:
-            with open('serie_db.txt', 'a+') as file:
-                file_pointer = file.read()
-                write_data = f'{self.selection}, {self.season_count}  \n'
+    #     else:
+    #         with open('serie_db.txt', 'a+') as file:
+    #             file_pointer = file.read()
+    #             write_data = f'{self.selection}, {self.season_count}  \n'
                 
-                if write_data in file:
-                    print('Show already exists in the database!')
+    #             if write_data in file:
+    #                 print('Show already exists in the database!')
                     
-                else:
-                    file.write(write_data)
-                    print ("Written show and it's season details to the database 😎.") 
+    #             else:
+    #                 file.write(write_data)
+    #                 print ("Written show and it's season details to the database 😎.") 
                     
-        x = input("Enter 'yes' to go back to main page or 'no'/'quit' to quit the program.  ")
+    #     x = input("Enter 'yes' to go back to main page or 'no'/'quit' to quit the program.  ")
         
-        if x == 'yes':
-            return self.ask_save()
-        else:
-            return sys.exit()
+    #     if x == 'yes':
+    #         return self.ask_save()
+    #     else:
+    #         return sys.exit()
     
-    
-class table(search):
+
+
+
+
+class table:
     
     def show_db(self):
         #making line_count to get the number of shows stored in text file
@@ -226,11 +187,33 @@ class table(search):
         series_db = tabulate(series_l, headers=['S.no.', 'Show Name' , 'Season Count'], tablefmt='fancy_grid', showindex=range(1, line_count+1))
         
         
-        return series_db
+        print(series_db)
                     
-
+def main():
+    launch = app().welcome()
+    
+    if launch == '1':
+        show_search = app().ask_input()
+        results = app().results(show_search)
+    
+    elif launch == '2':
+        show_db = table().show_db()
+        system('pause')
+        write_data = app().ask_save()
+    
+    elif launch == '3':
+        get_updates = webscraper().get_seasons()
+    
+    elif launch == 'quit':
+        sys.exit()
+    
+    else:
+      print('\n\nEnter a valid selection!\n\n')  
+      return False
+    
    
 if __name__ == '__main__':
-    search().ask_save()
-
-
+    
+    while True:
+        main()
+    
