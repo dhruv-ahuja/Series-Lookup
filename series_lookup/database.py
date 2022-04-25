2097eaf4886d1b1e6e936b9366ea413a902260e7
Dhruv <dhruvahuja2k@gmail.com>
2022-04-26 00:56:15 +0530

    refactor: update db functions
    - db functions no longer directly return errors

--- series_lookup/database.py
+++ series_lookup/database.py
@@ -3,22 +3,26 @@ from typing import Tuple
 import sqlite3
 
 
-def connect_to_db(db_path: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+def connect_to_db(db_path: str) -> sqlite3.Connection:
     """
     Forms a connection to a database given its path.
     """
 
     try:
         conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
     except sqlite3.Error as e:
-        return (None, None)
-    else:
-        return (conn, cursor)
+        print(e)
+    finally:
+        return conn
+
+
+# function to be used at the end of the application's execution
+def cleanup_db_connection(conn: sqlite3.Connection):
+    conn.close()
 
 
 # make_table is to be run during the pre-run check
-def make_table(cursor: sqlite3.Cursor) -> sqlite3.Error:
+def make_table(conn: sqlite3.Connection):
     """
     Creates the "show_data" table to be used for storing the app's data
     if it doesn't yet exist.
@@ -33,8 +37,9 @@ def make_table(cursor: sqlite3.Cursor) -> sqlite3.Error:
     """
 
     try:
+        cursor = conn.cursor()
         cursor.execute(query)
     except sqlite3.Error as e:
-        return e
-    else:
-        return None
+        print(e)
+    finally:
+        cursor.close()
