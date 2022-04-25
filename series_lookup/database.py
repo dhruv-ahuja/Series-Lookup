95aef2a6fa6c2bccd2d86276743197780004aeb9
Dhruv <dhruvahuja2k@gmail.com>
2022-04-26 00:22:12 +0530

    refactor: revamp connection process to database
    - remove the ContextManager class to connect to the database
    
    - use function for the same
    
    - update make_table function

--- series_lookup/database.py
+++ series_lookup/database.py
@@ -1,37 +1,27 @@
+from typing import Tuple
+
 import sqlite3
 
 
-class ContextManager:
+def connect_to_db(db_path: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
     """
-    Context manager to make interacting with the database easier.
-
-    Connects to the database, generates and returns the cursor to be used
-    when performing database-related operations.
+    Forms a connection to a database given its path.
     """
 
-    def __init__(self, db_path):
-        # file is our db file
-        self.db_path = db_path
-
-    def __enter__(self):
-        self.conn = sqlite3.connect(self.db_path)
-        self.cursor = self.conn.cursor()
-
-        # we won't be able to use any of the context manager's methods in the
-        # `with` block in other parts of our app if we do not return the class
-        # instance
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        # exc_type, exc_val and exc_tb refer to the exception type, exception
-        # value and exception traceback respectively
-        self.conn.close()
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+    except sqlite3.Error as e:
+        return (None, None)
+    else:
+        return (conn, cursor)
 
 
 # make_table is to be run during the pre-run check
-def make_table(db_path: str) -> sqlite3.Error:
+def make_table(cursor: sqlite3.Cursor) -> sqlite3.Error:
     """
-    Creates the "show_data" table to be used for storing the app's data if it doesn't yet exist.
+    Creates the "show_data" table to be used for storing the app's data
+    if it doesn't yet exist.
     """
 
     query = """
@@ -42,10 +32,9 @@ def make_table(db_path: str) -> sqlite3.Error:
     );
     """
 
-    with ContextManager(db_path) as db:
-        try:
-            db.cursor.execute(query)
-        except sqlite3.Error as e:
-            return e
-        else:
-            return None
+    try:
+        cursor.execute(query)
+    except sqlite3.Error as e:
+        return e
+    else:
+        return None
